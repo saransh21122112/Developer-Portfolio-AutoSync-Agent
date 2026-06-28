2d45381b9363f2ac3f2db9bb5c1562c0caf0df05
Saransh Gupta <saranshgupta2112@gmail.com>
2026-06-28 12:43:34 +0530

    fix: corrected relative import path of db.js in status route

--- dashboard/src/app/api/approvals/[project]/status/route.js
+++ dashboard/src/app/api/approvals/[project]/status/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { kv } from '@vercel/kv';
-import { isKvConfigured, readLocalDb, writeLocalDb } from '../../../../db';
+import { isKvConfigured, readLocalDb, writeLocalDb } from '../../../db';
 
 // GET status of a project
 export async function GET(request, { params }) {
